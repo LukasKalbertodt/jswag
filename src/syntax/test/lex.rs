c424f575ad24ec2082915ac8a2e3af3fdd402f0a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-09-14 21:01:21 +0200

    Add newline test and shorten other tests

--- src/syntax/test/lex.rs
+++ src/syntax/test/lex.rs
@@ -27,7 +27,7 @@ fn reals(src: &str) -> Vec<Token> {
 
 macro_rules! toks {
     ($s:expr, [$($v:expr),*]) => {
-        assert_eq!(toks($s), vec![$($v)*])
+        assert_eq!(toks($s), vec![$($v),*])
     }
 }
 macro_rules! reals {
@@ -39,22 +39,22 @@ macro_rules! reals {
 
 #[test]
 fn empty() {
-    assert_eq!(toks(""), vec![]);
+    toks!("", []);
 }
 
 #[test]
 fn idents() {
-    assert_eq!(toks("foo"), vec![Token::Ident("foo".into())]);
-    assert_eq!(toks("foo bar"), vec![
-        Token::Ident("foo".into()),
-        Token::Whitespace,
-        Token::Ident("bar".into()),
+    toks!("foo", [Ident("foo".into())]);
+    toks!("foo bar", [
+        Ident("foo".into()),
+        Whitespace,
+        Ident("bar".into())
     ]);
-    assert_eq!(toks("1bla"), vec![
-        Token::Literal(Lit::Integer("1".into(), false, 10)),
-        Token::Ident("bla".into())
+    toks!("1bla", [
+        Literal(Lit::Integer("1".into(), false, 10)),
+        Ident("bla".into())
     ]);
-    assert_eq!(toks("b1la"), vec![Token::Ident("b1la".into())]);
+    toks!("b1la", [Ident("b1la".into())]);
 
 }
 
@@ -85,27 +85,35 @@ fn ops() {
 }
 
 #[test]
+fn easy_literals() {
+    reals!("true false null", [
+        Literal(Lit::Bool(true)), Literal(Lit::Bool(false)), Literal(Lit::Null)
+    ]);
+    reals!("truefalse null", [Ident("truefalse".into()), Literal(Lit::Null)]);
+}
+
+#[test]
 fn int_literals() {
     assert_eq!(toks("123"), vec![
-        Token::Literal(Lit::Integer("123".into(), false, 10))
+        Literal(Lit::Integer("123".into(), false, 10))
     ]);
     assert_eq!(toks("123l"), vec![
-        Token::Literal(Lit::Integer("123".into(), true, 10))
+        Literal(Lit::Integer("123".into(), true, 10))
     ]);
     assert_eq!(toks("0123"), vec![
-        Token::Literal(Lit::Integer("123".into(), false, 8))
+        Literal(Lit::Integer("123".into(), false, 8))
     ]);
     assert_eq!(toks("0x1fa3l"), vec![
-        Token::Literal(Lit::Integer("1fa3".into(), true, 16))
+        Literal(Lit::Integer("1fa3".into(), true, 16))
     ]);
     assert_eq!(toks("0x1f"), vec![
-        Token::Literal(Lit::Integer("1f".into(), false, 16))
+        Literal(Lit::Integer("1f".into(), false, 16))
     ]);
     assert_eq!(toks("0b101l"), vec![
-        Token::Literal(Lit::Integer("101".into(), true, 2))
+        Literal(Lit::Integer("101".into(), true, 2))
     ]);
     assert_eq!(toks("0l"), vec![
-        Token::Literal(Lit::Integer("0".into(), true, 10))
+        Literal(Lit::Integer("0".into(), true, 10))
     ]);
 }
 
@@ -114,28 +122,28 @@ fn unicode_escapes() {
     // correct
     assert_eq!(spans(r"z\u0078z"), vec![
         TokenSpan {
-            tok: Token::Ident("zxz".into()),
+            tok: Ident("zxz".into()),
             span: Span { lo: 0, hi: 7 }
         }
     ]);
     // too few hex digits
     assert_eq!(spans(r"z\u00z"), vec![
         TokenSpan {
-            tok: Token::Ident("zz".into()),
+            tok: Ident("zz".into()),
             span: Span { lo: 0, hi: 5 }
         }
     ]);
     // value is not a valid unicode scalar
     assert_eq!(spans(r"z\udecez"), vec![
         TokenSpan {
-            tok: Token::Ident("zz".into()),
+            tok: Ident("zz".into()),
             span: Span { lo: 0, hi: 7 }
         }
     ]);
     // correct with multiple 'u's
     assert_eq!(spans(r"z\uuuu0078z"), vec![
         TokenSpan {
-            tok: Token::Ident("zxz".into()),
+            tok: Ident("zxz".into()),
             span: Span { lo: 0, hi: 10 }
         }
     ]);
@@ -143,51 +151,84 @@ fn unicode_escapes() {
     // currently the lexer stops at backslash... enable this test again later!
     // assert_eq!(spans(r"z\\uuuu0078z"), vec![
     //     TokenSpan {
-    //         tok: Token::Ident(r"z\\uuuu0078z".into()),
+    //         tok: Ident(r"z\\uuuu0078z".into()),
     //         span: Span { lo: 0, hi: 11 }
     //     }
     // ]);
 }
 
 #[test]
-fn basic_spans() {
-    assert_eq!(spans("abc xyz"), vec![
+fn new_lines() {
+    assert_eq!(spans("abc \n xyz"), vec![
         TokenSpan {
-            tok: Token::Ident("abc".into()),
+            tok: Ident("abc".into()),
             span: Span { lo: 0, hi: 2 }
         },
         TokenSpan {
-            tok: Token::Whitespace,
+            tok: Whitespace,
+            span: Span { lo: 3, hi: 5 }
+        },
+        TokenSpan {
+            tok: Ident("xyz".into()),
+            span: Span { lo: 6, hi: 8 }
+        }
+    ]);
+
+    assert_eq!(spans("abc \r\n xyz"), vec![
+        TokenSpan {
+            tok: Ident("abc".into()),
+            span: Span { lo: 0, hi: 2 }
+        },
+        TokenSpan {
+            tok: Whitespace,
+            span: Span { lo: 3, hi: 6 }
+        },
+        TokenSpan {
+            tok: Ident("xyz".into()),
+            span: Span { lo: 7, hi: 9 }
+        }
+    ]);
+}
+
+#[test]
+fn basic_spans() {
+    assert_eq!(spans("abc xyz"), vec![
+        TokenSpan {
+            tok: Ident("abc".into()),
+            span: Span { lo: 0, hi: 2 }
+        },
+        TokenSpan {
+            tok: Whitespace,
             span: Span { lo: 3, hi: 3 }
         },
         TokenSpan {
-            tok: Token::Ident("xyz".into()),
+            tok: Ident("xyz".into()),
             span: Span { lo: 4, hi: 6 }
         }
     ]);
 
     assert_eq!(spans(".xxxx=="), vec![
         TokenSpan {
-            tok: Token::Dot,
+            tok: Dot,
             span: Span { lo: 0, hi: 0 }
         },
         TokenSpan {
-            tok: Token::Ident("xxxx".into()),
+            tok: Ident("xxxx".into()),
             span: Span { lo: 1, hi: 4 }
         },
         TokenSpan {
-            tok: Token::EqEq,
+            tok: EqEq,
             span: Span { lo: 5, hi: 6 }
         }
     ]);
 
     assert_eq!(spans("     !"), vec![
         TokenSpan {
-            tok: Token::Whitespace,
+            tok: Whitespace,
             span: Span { lo: 0, hi: 4 }
         },
         TokenSpan {
-            tok: Token::Bang,
+            tok: Bang,
             span: Span { lo: 5, hi: 5 }
         }
     ]);
