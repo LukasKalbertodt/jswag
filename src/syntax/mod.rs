1f819c4e6d5f6290716da8fafb50d41138918b62
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-01-27 16:15:33 +0100

    Start anew with xbase system (delete everything)

--- src/syntax/mod.rs
+++
@@ -1,9 +0,0 @@
-mod lex;
-mod token;
-pub mod ast;
-pub mod parse;
-
-pub use self::token::*;
-pub use self::lex::Tokenizer;
-
-mod test;
--- src/syntax/test/mod.rs
+++
@@ -1,4 +0,0 @@
-/// This module contains a bunch of unit tests for functions in the syntax
-/// module.
-
-mod lex;
