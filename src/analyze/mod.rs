5b5cfb8e4474b7177eb23050d5d8227eba22dcee
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-03-09 17:23:23 +0100

    Add method name checking

--- src/analyze/mod.rs
+++ src/analyze/mod.rs
@@ -1,3 +1,6 @@
+// BEWARE: low quality shit code ahead
+// TODO: make americode great again
+
 use syntax::{ast};
 use base::{diag, code};
 use syntax::ast::ItemExt;
@@ -6,6 +9,14 @@ pub fn check_names(ast: &ast::CompilationUnit, file: &code::FileMap) {
     for ty in &ast.types {
         if let Some(ref ident) = ty.ident() {
             check_upper_camel_case(ident, "type name", file);
+        }
+
+        if let &ast::TypeDef::NormalClass(ref c) = ty {
+            for member in &c.members {
+                if let &ast::ClassMember::Method(ref method) = member {
+                    check_lower_camel_case(&method.name, "method name", file);
+                }
+            }
         }
     }
 
@@ -36,6 +47,51 @@ fn check_upper_camel_case(name: &ast::Ident, context: &str, file: &code::FileMap
 
         let rep = diag::Report::simple_warning(
             format!("This {} should be written in 'UpperCamelCase'", context),
+            name.span,
+        ).with_remark(diag::Remark::note(
+            "Consider changing the name like shown here",
+            diag::Snippet::Replace {
+                span: name.span,
+                with: new_name,
+            }
+        ));
+        diag::print(&rep, &file, diag::PrintOptions::default());
+    }
+}
+
+fn check_lower_camel_case(name: &ast::Ident, context: &str, file: &code::FileMap) {
+    // TODO unwrap
+    let first_is_uppercase = name.name.chars().next().unwrap().is_uppercase();
+    let has_underscores = name.name.find('_').is_some();
+    if first_is_uppercase || has_underscores {
+        let mut needs_uppercase = false;
+        let mut new_name: String = name.name.chars().skip(1).filter_map(|c| {
+            if c == '_' {
+                needs_uppercase = true;
+                None
+            } else if c.is_lowercase() && needs_uppercase {
+                let up: Vec<_> = c.to_uppercase().collect();
+                needs_uppercase = false;
+                if up.len() == 1 {
+                    Some(up[0])
+                } else {
+                    Some(c)
+                }
+            } else {
+                Some(c)
+            }
+        }).collect();
+
+        let first_lower: Vec<_> = name.name.chars().next().unwrap().to_lowercase().collect();
+        let first_lower = if first_lower.len() == 1 {
+            first_lower[0]
+        } else {
+            name.name.chars().next().unwrap()
+        };
+        new_name.insert(0, first_lower);
+
+        let rep = diag::Report::simple_warning(
+            format!("This {} should be written in 'lowerCamelCase'", context),
             name.span,
         ).with_remark(diag::Remark::note(
             "Consider changing the name like shown here",
